1fecee6c8bc5dcc4021f6f9d6580a870e7697993
AzrielNovan <72910953+AzrielNovan@users.noreply.github.com>
2021-06-07 11:07:50 +0700

    Add files via upload

---
+++ class1/TypeDataKembalian.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class1;
+
+/**
+ *
+ * @author dell
+ */
+class TypeDataKembalian {
+
+    // membuat fungsi ucapSalam()
+    static void ucapSalam(){
+        System.out.println("Selamat Pagi");
+        System.out.println("Perkenalkan nama saya : Muhammad Azriel Novansyah");
+        System.out.println("dari kelas : X RPL 2");
+        System.out.println("Saya berasal dari malang ");
+        System.out.println("Saya lahir pada tanggal 13 Januari 2005, Malang");
+    }
+
+    // membuat fungsi main()
+    public static void main(String[] args){
+        // memanggil/eksekusi fungsi ucapSalam()
+        ucapSalam();
+    }
+
+}
+
